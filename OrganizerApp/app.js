91bc9adef2243b4da23ea778a873c271c47f717d
andrej <andrej@eidosearch.com>
2015-03-16 01:01:00 -0400

    Added new templates

--- OrganizerApp/app.js
+++ OrganizerApp/app.js
@@ -1,5 +1,5 @@
 /**
- * Created by aknezevs on 12/03/15.
+ * Created by andrej on 12/03/15.
  */
 angular.module('underscore', [])
   .factory('_', function() {
@@ -39,11 +39,13 @@ angular.module('OrganizerApp')
 
     // https://api.mongolab.com/api/1/databases/{database}/collections/{collection}?apiKey=myAPIKey
     var resourceUrl = 'https://api.mongolab.com/api/1/databases/' + dbName + '/collections/' + collectionName + '/:id';
+    console.log(resourceUrl);
     return $resource(resourceUrl, {
       apiKey: apiKey,
-      id:'@_id$.oid'
+      id:'@_id.$oid'
     }, {
-      update: {method:'PUT'}
+      update: {method:'PUT', isArray: false},
+      delete: {method:'DELETE', isArray: false}
     });
   })
 
@@ -51,16 +53,18 @@ angular.module('OrganizerApp')
     var contactsService = {};
 
     contactsService.getContacts = function(Contacts) {
-      var contacts = Contacts.query();
+      var contacts = Contacts.query()
+        .$promise
+        .then(function(result) {
+          return result;
+        });
       return contacts;
     };
 
-    contactsService.findContacts = function(properties) {
-      return _.findWhere(contacts, properties);
-    };
-
     contactsService.isValidContact = function(contact) {
-      return (contact.hasOwnProperty('name') &&
+      return (!_.isUndefined(contact) &&
+        !_.isNull(contact) &&
+        contact.hasOwnProperty('name') &&
         contact.hasOwnProperty('phoneNumber'));
     };
 
@@ -71,25 +75,77 @@ angular.module('OrganizerApp')
       Contacts.save(contact);
     };
 
-    contactsService.modifyContact = function(properties, newContact) {
-      var toBeModified = contactsService.findContacts({contactId: properties.contactId});
-      toBeModified.name = newContact.name;
-      toBeModified.phoneNumber = newContact.phoneNumber;
+    contactsService.modifyContact = function(contact) {
+      return contact.$update({name: contact.name, phoneNumber: contact.phoneNumber})
+        .then(function(modifiedContact) {
+          return modifiedContact;
+        })
+        .then(null, function(error) {
+          console.log(error);
+          throw new Error('Could not update contact');
+        });
+    };
+
+    contactsService.deleteContact = function(contact) {
+      return contact.$delete()
+        .then(function(res) {
+          return res;
+        })
+        .then(null, function(error) {
+          console.log(error);
+          throw new Error('Could not delete contact');
+        });
     };
 
     return contactsService;
   }])
 
-  .controller('ContactsListCtrl', function(contactsService, Contacts) {
+  .controller('ContactsListCtrl', function($scope, $q,$timeout, contactsService, Contacts) {
     var vm = this;
-    vm.contacts = contactsService.getContacts(Contacts);
+    var flag = true;
+
+    vm.updateContactListView = function (contactToShow) {
+      if (!_.isUndefined(contactToShow)) {
+        vm.contacts = [contactToShow];
+      } else {
+        contactsService.getContacts(Contacts)
+          .then(function (contacts) {
+            vm.contacts = contacts;
+            console.log('update contacts view', vm.contacts);
+          });
+      }
+    };
+
+    vm.deleteContact = function (contact) {
+      contactsService.deleteContact(contact, Contacts)
+        .then(function () {
+          console.log('Delete UPDATED');
+          vm.updateContactListView();
+          flag = false;
+        });
+    };
+
+    vm.modifyContact = function (contact) {
+      console.log(contact);
+      contactsService.modifyContact(contact)
+        .then(function () {
+          console.log('Modify UPDATED');
+          vm.updateContactListView();
+          flag = false;
+        });
+    };
+
+    // update the contacts list view
+    vm.updateContactListView();
   })
   .controller('ContactAddCtrl', function(contactsService, Contacts) {
     var vm = this;
     vm.addContact = function () {
-      console.log('adding new contact ' + vm.newContact.name);
       if(contactsService.isValidContact(vm.newContact)) {
+        console.log('Adding new contact ' + vm.newContact.name);
         contactsService.addContact(vm.newContact, Contacts);
+      } else {
+        console.log('Cannot add contact');
       }
     };
   });
